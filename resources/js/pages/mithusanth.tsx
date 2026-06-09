fae3e47092ee7cb7e74c4300b401cd0d33d9e4e1
Jack-Mithu <mithushanthjack@gmail.com>
2026-06-09 11:13:53 +0100

    bit change in web

--- resources/js/pages/mithusanth.tsx
+++ resources/js/pages/mithusanth.tsx
@@ -10,7 +10,7 @@ const ME = {
     name: 'Mithusanth Sukumar',
     role: 'Full-Stack Developer',
     bio: 'Building scalable web & mobile applications with Laravel, React, and React Native. I care about clean architecture, fast UIs, and products that actually solve problems.',
-    email: 'mithusanth@gmail.com',
+    email: 'mithusanthjack@gmail.com',
     github: 'https://github.com/Jack-Mithu',
     linkedin: 'https://linkedin.com/in/sukumar-mithushanth',
     image: '/images/profile.jpg',
